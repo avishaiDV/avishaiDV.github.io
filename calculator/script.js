be1ebad794d029058149fe54756704b3956b1689
avishaiDV <39707337+avishaiDV@users.noreply.github.com>
2021-02-25 13:14:24 +0200

    Update script.js
    bug fixes and stabilization.

--- calculator/script.js
+++ calculator/script.js
@@ -4,22 +4,25 @@ let screen = document.getElementById('screen');
 let clear = document.getElementById('C');
 let eq = document.getElementById('=');
 let ans = document.getElementById('ans');
-let result ='', temp = 0;
+let result = '',
+    temp = 0;
+const error = 'Math error :/';
 
 sign.forEach(s => {
     s.addEventListener('click', () => {
-        if(result != ''){
+        if (result != '') {
             clear.click();
             result = '';
             s.click();
         } else {
             screen.value += s.innerText;
-        }    });
+        }
+    });
 });
 
 num.forEach(n => {
     n.addEventListener('click', () => {
-        if(result != ''){
+        if (result != '' || screen.value == error) {
             clear.click();
             result = '';
             n.click();
@@ -35,19 +38,24 @@ clear.onclick = () => {
 
 eq.onclick = () => {
     result = eval(screen.value);
-    if(result === undefined || result == Infinity){
-        result = 'Math error :/';
+    if (result === undefined || result == Infinity) {
+        result = error;
     }
     screen.value = result;
-    temp = result;
+    if (result == error) {
+        temp = 0;
+    } else {
+        temp = result;
+    }
+    result = '';
 }
 
 ans.onclick = () => {
-    if(result != ''){
+    if (screen.value == error) {
         clear.click();
         result = '';
         ans.click();
     } else {
         screen.value += temp;
-}
+    }
 }
